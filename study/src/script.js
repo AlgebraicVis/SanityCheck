fca1707be9cd6c8401b657e1a9ab293c51fb5301
mcorrell <michael.a.correll@gmail.com>
2018-03-07 18:24:18 -0800

    Added simple vis types

--- study/src/script.js
+++ study/src/script.js
@@ -12,6 +12,8 @@ var vizColumns = 4;
 var vizWidth = 200;
 var vizHeight = 100;
 
+var x = d3.scaleLinear().domain([0,1]).range([0,vizWidth]);
+
 /***
 UI Functions
 ***/
@@ -78,8 +80,66 @@ var select = function(){
 };
 
 /***
+Viz Functions
+***/
+
+var scatter = function(svg,data,markSize,markOpacity){
+  svg.selectAll("circle").data(data).enter().append("circle")
+    .attr("fill","#333")
+    .attr("r",markSize)
+    .attr("cx", d => x(d))
+    .attr("cy", vizHeight/2)
+    .attr("opacity",markOpacity);
+};
+
+function testScatter(){
+  scatter(d3.select("svg"),dl.random.uniform(0,1).samples(50),10,0.2);
+}
+
+//Makes a histogram
+//Takes a target svg, a data set, and a bin size.
+var histogram = function(svg,data,binCount){
+  var bins = dl.histogram(data,{min: 0,max:1,step: 1/binCount});
+  var width = Math.ceil(x(bins.bins.step));
+  var y = d3.scaleLinear().domain([0,dl.max(bins,"count")]).range([vizHeight,0]);
+
+  svg.selectAll("rect").data(bins).enter().append("rect")
+    .attr("x",d => Math.floor(x(d.value)) )
+    .attr("y", d => y(d.count))
+    .attr("fill","#333")
+    .attr("height", d => vizHeight - y(d.count))
+    .attr("width",width);
+};
+
+function testHistogram(){
+  histogram(d3.select("svg"),dl.random.uniform(0,1).samples(50),20);
+};
+
+//Makes a density plot
+//Takes a target svg, a data set, and a kernel bandwidth
+var density = function(svg,data,bandwidth){
+  var kde = KDE(data,bandwidth);
+  var xs = x.ticks(40);
+  var densities = xs.map(function(d){ return {x: d, y: kde(d)}});
+  var y = d3.scaleLinear().domain([0,dl.max(densities,"y")]).range([vizHeight,0]);
+  var area = d3.area()
+    .x(d => x(d.x))
+    .y0(y(0))
+    .y1(d => y(d.y));
+
+  svg.append("path").datum(densities)
+    .attr("fill","#333")
+    .attr("d", area);
+};
+
+function testDensity(){
+  density(d3.select("svg"),dl.random.uniform(0,1).samples(50),0.05);
+};
+/***
 Utility Functions
 ***/
+
+
 
 //Kullback-Leibler divergence D_KL(p||q) for 2 discrete distributions p and q
 var KLD = function(p,q){
@@ -90,4 +150,23 @@ var KLD = function(p,q){
   var qN = q.map(d => d/qSum);
   var info = pN.map(function(d,i){ return d*Math.log(d/qN[i]);});
   return dl.sum(info);
+};
+
+//Kernel Density Estimation
+
+//gaussian kernel
+var kernelGauss = function(sigma){
+  return function(pos){
+    var exp = Math.exp(Math.pow(pos, 2) / (-2 * Math.pow(sigma, 2)));
+    return (1 / (sigma * Math.sqrt(2*Math.PI))) * exp;
+  }
+};
+
+//returns a function that estimates the density at a given position
+var KDE = function(data,bandwidth){
+  var kernel = kernelGauss(bandwidth);
+  //sum up the contribution of each kernel, then divide by the number of kernels.
+  return function(pos){
+    return  dl.mean(data.map(d => kernel(d-pos)));
+  };
 };
